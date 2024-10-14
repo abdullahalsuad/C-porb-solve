7afb777fd81f0017f4ec307f73f58f7c0a0bff04
Abdullah Al Suad <suadabdullahal@gmail.com>
2024-10-15 01:15:23 +0600

    good night

--- digit.c
+++ digit.c
@@ -2,4 +2,38 @@
 
 int main()
 {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        scanf("%d", &num);
+
+        if (num == 0)
+        {
+            printf("0");
+        }
+
+        // int temp = num, digitCount = 0;
+        // for (; temp > 0; temp /= 10)
+        // {
+        //     digitCount++;
+        // }
+
+        // for (int j = 0; j < digitCount; j++)
+        // {
+        //     int digit = num % 10;
+        //     printf("%d ", digit);
+        //     num /= 10;
+        // }
+        while (num > 0)
+        {
+            int digit = num % 10;
+            printf("%d ", digit);
+            num /= 10;
+        }
+        printf("\n");
+    }
+    return 0;
 }
--- luckynum.c
+++ luckynum.c
@@ -9,7 +9,7 @@ int main()
     if (num >= 10 && num <= 99)
     {
         reNum = num % 10;
-        quNum = (int)num / 10;
+        quNum = num / 10;
         // printf("reNum = %d, quNum = %d", reNum, quNum);
 
         if (reNum % quNum == 0 || quNum % reNum == 0)
