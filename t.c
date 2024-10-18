45832131b0a00f6875604d3327968ec3992f6f5d
Abdullah Al Suad <suadabdullahal@gmail.com>
2024-10-18 12:12:14 +0600

    up

---
+++ t.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    if (num == 1)
+    {
+        printf("NO");
+    }
+
+    if (num % num == 0)
+    {
+        printf("YES")
+    }
+    else
+    {
+        printf("NO")
+    }
+}
