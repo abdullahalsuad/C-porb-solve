b1210019f82462f459ded58b438ab95e8d3f173d
Abdullah Al Suad <suadabdullahal@gmail.com>
2024-11-01 22:11:52 +0600

    up

--- assigement 3/dynamicarray7.c
+++ assigement 3/dynamicarray7.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
 int main()
 {
 
     int n;
     scanf("%d", &n);
-    int arr[n];
+    int *arr = (int *)malloc(1 * sizeof(int));
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < 1; i++)
     {
         scanf("%d", &arr[i]);
     }
-    for (int j = 0; j < n; j++)
+
+    arr = (int *)realloc(arr, n * sizeof(int));
+
+    for (int i = 1; i < n; i++)
     {
-        printf("%d ", arr[j]);
+        scanf("%d", &arr[i]);
     }
 
-        return 0;
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    free(arr);
+    return 0;
 }
